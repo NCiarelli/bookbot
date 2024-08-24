6e700d2dc992df79ff411ce75e2bcf55b97eaad1
NCiarelli <nicholas.ciarelli@gmail.com>
2024-08-24 08:14:32 -0400

    Finished letter sort

--- main.py
+++ main.py
@@ -11,11 +11,29 @@ def main():
 
     # Count the words in the book
     num_words = count_words(book_string)
-    print(f"Word count of {book_name}: {num_words}")
+    # print(f"Word count of {book_name}: {num_words}")
     
     # Count the characters in the book and create a dictionary of the counts
     char_count_dict = get_char_count_dict(book_string)
-    print(char_count_dict)
+    # print(char_count_dict)
+
+    # Generate and print a book stats report
+    sorted_letter_list = get_sorted_letter_list(char_count_dict)
+    print(sorted_letter_list)
+    
+
+
+def get_sorted_letter_list(char_count_dict):
+    
+    def sort_on(dict):
+        return dict["count"]
+    letter_list = []
+    for char_key in char_count_dict:
+        if char_key.isalpha():
+            letter_list.append({"letter": char_key, "count": char_count_dict[char_key]})
+    letter_list.sort(reverse=True, key=sort_on)
+    return letter_list
+    
 
 def get_char_count_dict(book_string):
     book_lower = book_string.lower()
